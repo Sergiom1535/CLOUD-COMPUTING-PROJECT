6cc19aa592639d88601828913130b629737c9314
Sergiom1535 <71371817+Sergiom1535@users.noreply.github.com>
2022-11-23 20:52:10 -0800

    Update connect_db.php
    hosted to aws rds

--- connect_db.php
+++ connect_db.php
@@ -1,4 +1,4 @@
 <?php
-	$dbc = mysqli_connect('localhost','root','','forum')OR die (mysqli_connect_error());
+	$dbc = mysqli_connect('testdb.ccvnbnct7vxa.us-west-1.rds.amazonaws.com','admin','Pa$$w0rd','Forum')OR die (mysqli_connect_error());
 	mysqli_set_charset($dbc,'utf8');
 ?>
